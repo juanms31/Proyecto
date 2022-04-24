840ff69ff0e74e2bde947a9daf5ba2ebde7c5228
JuanGomez <esetaljuane@gmail.com>
2022-04-24 12:55:50 +0200

    creacion de clases intermedias MaterialCompradoProveedor y MaterialUtilizadoActuacion

---
+++ src/com/company/Entidades/MaterialCompradoProveedor.java
@@ -0,0 +1,12 @@
+package com.company.Entidades;
+
+import java.util.Date;
+
+public class MaterialCompradoProveedor {
+    private int id;
+    private int id_materiales;
+    private int id_proveedores;
+    private int id_actuacion;
+    private int id_albaran;
+    private Date fecha_compra;
+}
---
+++ src/com/company/Entidades/MaterialUtilizadoActuacion.java
@@ -0,0 +1,7 @@
+package com.company.Entidades;
+
+public class MaterialUtilizadoActuacion {
+    private int id;
+    private int id_material;
+    private int id_actuacion;
+}
