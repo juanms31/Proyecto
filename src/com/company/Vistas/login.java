1d83156c8b37a06e0d86cc95f6f0b88efa054d8f
Juan <juanms3199@gmail.com>
2022-04-28 19:03:06 +0200

    Vista welcome creada.

---
+++ src/com/company/Vistas/login.java
@@ -0,0 +1,10 @@
+package com.company.Vistas;
+
+import javax.swing.*;
+
+public class login {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+    private JButton button2;
+}
---
+++ src/com/company/Vistas/welcome.java
@@ -0,0 +1,7 @@
+package com.company.Vistas;
+
+import javax.swing.*;
+
+public class welcome {
+    private JButton entrarButton;
+}
