38c0b4e781143c7157b6a731c954f20402cb7481
JuanGomez <esetaljuane@gmail.com>
2022-04-28 19:54:02 +0200

    add evento teclado para tecla enter en clase Welcome

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import com.company.Vistas.Welcome;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("eeee");
+        Welcome welcome = new Welcome();
+        welcome.setVisible(true);
     }
 }
--- src/com/company/Vistas/Welcome.java
+++ src/com/company/Vistas/Welcome.java
@@ -3,6 +3,8 @@ package com.company.Vistas;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Welcome extends JFrame{
     private JButton btnEnter;
@@ -18,6 +20,15 @@ public class Welcome extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 Login login = new Login();
                 dispose();
+            }
+        });
+        this.getContentPane().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER){
+                    Login login = new Login();
+                    dispose();
+                }
             }
         });
     }
