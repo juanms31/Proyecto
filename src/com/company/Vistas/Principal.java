e98c58f217382c816ff082890c8566858c2324b1
Juan <juanms3199@gmail.com>
2022-04-29 19:03:06 +0200

    Ventana principal creada.

--- src/com/company/Vistas/Principal.java
+++ src/com/company/Vistas/Principal.java
@@ -3,6 +3,8 @@ package com.company.Vistas;
 import javax.swing.*;
 
 public class Principal {
-    private JButton button1;
-    private JButton button2;
+    private JButton home;
+    private JPanel panelBotones;
+    private JPanel principal;
+    private JButton btnMateriales;
 }
