1adf26ac6f58d5f9929396e4143bbe38fc728c67
Juan <juanms3199@gmail.com>
2022-04-29 11:01:36 +0200

    Vista login y confirmacion creadas.

--- src/com/company/Vistas/Login.java
+++ src/com/company/Vistas/Login.java
@@ -2,22 +2,12 @@ package com.company.Vistas;
 
 import javax.swing.*;
 
-public class Login extends JFrame{
+public class Login {
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JButton entrarButton;
     private JButton cancelarButton;
-    private JPanel jpLogin;
-
-    public Login(){
-        initWindows();
-        add(jpLogin);
-    }
-
-    private void initWindows() {
-        //BASICO
-        setSize(600,400);
-        setLocationRelativeTo(null);
-        setResizable(false);
-    }
+    private JLabel msj_error;
+    private JLabel email;
+    private JLabel pass;
 }
