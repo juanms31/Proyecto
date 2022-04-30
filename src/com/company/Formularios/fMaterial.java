5b0dbe5a234cefa13f1d2d8602b5d3f5f6b4e59a
juanms31 <juanms3199@gmail.com>
2022-04-30 21:49:33 +0200

    Formulario material creado.

---
+++ src/com/company/Formularios/fMaterial.java
@@ -0,0 +1,15 @@
+package com.company.Formularios;
+
+import javax.swing.*;
+
+public class fMaterial {
+    private JButton button1;
+    private JButton button2;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JSpinner spinner3;
+}
