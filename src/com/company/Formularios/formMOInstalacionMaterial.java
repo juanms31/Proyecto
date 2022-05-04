da923b4f87b29be7c5714823f4dccbb1dd0b8357
Juan <juanms3199@gmail.com>
2022-05-04 20:10:44 +0200

    Creacion de formularios.

---
+++ src/com/company/Formularios/formMOInstalacionMaterial.java
@@ -0,0 +1,13 @@
+package com.company.Formularios;
+
+import javax.swing.*;
+
+public class formMOInstalacionMaterial {
+    private JButton aceptarButton;
+    private JButton cancelarButton;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+}
