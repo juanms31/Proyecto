ce0833ee3c28ad89c20087ef173b5c69a7478fbe
JuanGomez <esetaljuane@gmail.com>
2022-05-23 21:21:18 +0200

    create de CRUDUnidadMaterial.java hecho

--- src/com/company/BaseDatos/CRUDUnidadMaterial.java
+++ src/com/company/BaseDatos/CRUDUnidadMaterial.java
@@ -1,12 +1,10 @@
 package com.company.BaseDatos;
 
+import com.company.Entidades.CalidadMaterial;
 import com.company.Entidades.EspecificacionMaterial;
 import com.company.Entidades.UnidadMaterial;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CRUDUnidadMaterial {
@@ -35,8 +33,40 @@ public class CRUDUnidadMaterial {
             BBDD.close();
             return  null;
         }
-
     }
+
+    public int createUnidadMaterial(UnidadMaterial unidadMaterial) throws SQLException {
+        Connection connection = BBDD.connect();
+        if (connection == null) return -1;
+        final String QUERY_INSERT = "INSERT INTO unidadmaterial" +
+                " VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY_INSERT, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setNull(1, 1);
+            preparedStatement.setString(2, unidadMaterial.getSiglasUnidad());
+            preparedStatement.setString(3, unidadMaterial.getNombreUnidad());
+            preparedStatement.setString(4, unidadMaterial.getDescripcion());
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows == 0) throw new SQLException("No se pudo guardar");
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            int idRowUnidadMaterial = 0;
+            if(generatedKeys.next()){
+                idRowUnidadMaterial = generatedKeys.getInt(1);
+            }
+            BBDD.close();
+            return idRowUnidadMaterial;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            BBDD.close();
+            return  -1;
+        } finally {
+            if (!connection.isClosed()){
+                BBDD.close();
+            }
+        }
+    }
+
 
     //endregion
 
