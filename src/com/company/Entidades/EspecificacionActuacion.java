7ad3e23738b77053702db4e3a41688a57f00a8b2
juanms31 <juanms3199@gmail.com>
2022-05-26 17:01:47 +0200

    Form Actuacion completado.

---
+++ src/com/company/Entidades/EspecificacionActuacion.java
@@ -0,0 +1,48 @@
+package com.company.Entidades;
+
+public class EspecificacionActuacion {
+    //region SETTER AND GETTER
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSiglasEspecificacion() {
+        return siglasEspecificacion;
+    }
+
+    public void setSiglasEspecificacion(String siglasEspecificacion) {
+        this.siglasEspecificacion = siglasEspecificacion;
+    }
+
+    public String getNombreEspecificacion() {
+        return nombreEspecificacion;
+    }
+
+    public void setNombreEspecificacion(String nombreEspecificacion) {
+        this.nombreEspecificacion = nombreEspecificacion;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    //endregion
+
+    //region ATRIBUTOS
+
+    private int id;
+    private String siglasEspecificacion;
+    private String nombreEspecificacion;
+    private String Descripcion;
+
+    //endregion
+}
